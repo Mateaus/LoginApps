207874e8586b76dd1a5578ced55b2fbbceb20c65
Mateaus <mateausc@gmail.com>
2018-05-21 22:36:49 -0500

    new activity

---
+++ app/src/main/java/com/example/mat/systemmanagement/CreateUser.java
@@ -0,0 +1,4 @@
+package com.example.mat.systemmanagement;
+
+public class CreateUser {
+}
---
+++ app/src/main/java/com/example/mat/systemmanagement/RecyclerActivity.java
@@ -0,0 +1,34 @@
+package com.example.mat.systemmanagement;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    private static final String TAG = "RecyclerActivity";
+    
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: pressed!"); // Whenever mail icon is pressed, it shows it in the Logcat
+            }
+        });
+
+    }
+
+}
